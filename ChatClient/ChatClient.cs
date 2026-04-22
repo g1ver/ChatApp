4fa4b304d77742a801c8552f58b789577cd0ff89
g1ver <c0ping@pm.me>
2026-04-22 11:23:02 -0700

    initial commit

---
+++ ChatClient/ChatClient.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient;
+
+internal static class ChatClient
+{
+    private const int Port = 6767;
+    private const string Server = "127.0.0.1";
+
+    private static void Main()
+    {
+        using var client = new TcpClient(Server, Port);
+        var stream = client.GetStream();
+
+        Console.WriteLine($"Connected to {Server}:{Port}");
+
+        HandleMessages(stream);
+
+        Console.Write("> ");
+        while (client.Connected)
+        {
+            var input = Console.ReadLine();
+            if (input is null) break;
+            if (input.Length == 0)
+            {
+                Console.Write("> ");
+                continue;
+            }
+
+            byte[] data;
+            var quit = false;
+            if (input is "!exit")
+            {
+                data = "COMMAND:QUIT\n"u8.ToArray();
+                quit = true;
+            }
+            else
+            {
+                data = Encoding.UTF8.GetBytes(input + '\n');
+            }
+
+            stream.Write(data, 0, data.Length);
+            if (quit) break;
+            Console.Write("> ");
+        }
+    }
+
+    private static void HandleMessages(Stream stream)
+    {
+        Task.Run(() =>
+        {
+            using var reader = new StreamReader(stream, Encoding.UTF8);
+            while (reader.ReadLine() is { } line)
+            {
+                Console.Write("\r" + new string(' ', Console.WindowWidth) + "\r");
+                Console.WriteLine(line);
+                Console.Write("> ");
+            }
+        });
+    }
+}
---
+++ ChatServer/ChatServer.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer;
+
+internal static class ChatServer
+{
+    private const int Port = 6767;
+    private const string ServerIp = "127.0.0.1";
+
+    private static readonly ConcurrentDictionary<TcpClient, string> Clients = [];
+
+    private static void Main()
+    {
+        TcpListener? server = null;
+        try
+        {
+            var localAddr = IPAddress.Parse(ServerIp);
+            server = new TcpListener(localAddr, Port);
+            server.Start();
+
+            Console.WriteLine("Waiting for a connections...");
+            while (true)
+            {
+                var client = server.AcceptTcpClient();
+                Task.Run(() => HandleClient(client));
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+        finally
+        {
+            server?.Stop();
+        }
+    }
+
+    private static void HandleClient(TcpClient client)
+    {
+        SendWelcomeMessage(client);
+
+        try
+        {
+            var stream = client.GetStream();
+            using var reader = new StreamReader(stream, Encoding.UTF8);
+
+            var username = PromptUsername(client, reader);
+            if (username == string.Empty) return;
+
+            SendMessage(client, $"Server: Your username is: {username}.");
+            BroadcastClientJoinMessage(client, username);
+            AddClient(client, username);
+
+            Console.WriteLine($"Client connected: {username} ({client.Client.RemoteEndPoint})");
+
+            while (reader.ReadLine() is { } data)
+            {
+                if (data.Equals("COMMAND:QUIT")) break;
+                BroadcastClientMessage(client, data);
+                Console.WriteLine($"{GetUsername(client)} ({client.Client.RemoteEndPoint}): {data}");
+            }
+
+            BroadcastClientLeaveMessage(client);
+            Console.WriteLine($"Client disconnected: {username} ({client.Client.RemoteEndPoint})");
+            RemoveClient(client);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: {0}", e);
+        }
+    }
+
+    private static string PromptUsername(TcpClient client, StreamReader reader)
+    {
+        SendMessage(client, "Server: Enter your username below.");
+
+        while (reader.ReadLine() is { } data)
+        {
+            if (!string.IsNullOrWhiteSpace(data) &&
+                !Clients.Values.Any(u => u.Equals(data, StringComparison.OrdinalIgnoreCase))) return data;
+            SendMessage(client, "Server: That username is invalid or already in use! Try again.");
+        }
+
+        return string.Empty; // client disconnected before choosing a name
+    }
+
+    private static void BroadcastMessage(string message)
+    {
+        foreach (var (client, _) in GetClientsSnapshot())
+        {
+            SendMessage(client, message);
+        }
+    }
+
+    private static void BroadcastClientMessage(TcpClient client, string message)
+    {
+        BroadcastMessage($"{GetUsername(client)}: {message}");
+    }
+
+    private static IEnumerable<KeyValuePair<TcpClient, string>> GetClientsSnapshot()
+    {
+        return Clients.ToArray();
+    }
+
+    private static void SendMessage(TcpClient client, string message)
+    {
+        var data = Encoding.UTF8.GetBytes($"{message}\n");
+        client.GetStream().Write(data, 0, data.Length);
+    }
+
+    private static void BroadcastClientJoinMessage(TcpClient client, string username)
+    {
+        BroadcastMessage($"Server: {username} ({client.Client.RemoteEndPoint}) has joined the chat.");
+    }
+
+    private static void BroadcastClientLeaveMessage(TcpClient client)
+    {
+        BroadcastMessage($"Server: {GetUsername(client)} ({client.Client.RemoteEndPoint}) has left the chat.");
+    }
+
+    private static void SendWelcomeMessage(TcpClient client)
+    {
+        SendMessage(client, $"Server: Welcome to the chat! You are connecting from {client.Client.RemoteEndPoint}.");
+    }
+
+    private static void AddClient(TcpClient client, string username)
+    {
+        Clients.TryAdd(client, username);
+    }
+
+    private static void RemoveClient(TcpClient client)
+    {
+        Clients.TryRemove(client, out _);
+        client.Close();
+    }
+
+    private static string GetUsername(TcpClient client)
+    {
+        return Clients.GetValueOrDefault(client, client.Client.RemoteEndPoint?.ToString() ?? "Unknown");
+    }
+}
